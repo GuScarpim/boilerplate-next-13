f9a72199511c6422f7d6cb837137dda616f889dc
guscarpim <gustavoscarpim@gmail.com>
2023-03-25 16:55:06 -0300

    Add home

---
+++ boilerplate-next/src/app/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Boilerplate Next 13 - GuScarpim'
+}
+
+export default function Home() {
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <h1 className="text-3xl font-semibold text-center">
+        Boilerplate Gustavo Scarpim Next 13
+      </h1>
+    </div>
+  )
+}
